c82b49d87581c575dd62f8e3d180375a73e85a1a
osmonalievv <osmonalievbekzhann@gmail.com>
2021-12-01 01:19:04 +0600

    commit128

--- app/src/main/java/com/example/myapplication/MainActivityAncient.kt
+++ app/src/main/java/com/example/myapplication/MainActivityAncient.kt
@@ -1,11 +1,14 @@
 package kg.tsi.geohis
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivityAncient : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_ancient)
@@ -14,9 +17,19 @@ class MainActivityAncient : AppCompatActivity() {
         //body
         val click1 = findViewById<Button>(R.id.but_train)
 
-
         click1.setOnClickListener {
             startActivity(Intent(this, MainActivityGeo_memory::class.java))
+
+
+            val click2 = findViewById<ImageButton>(R.id.imageView2)
+
+            click2.setOnClickListener {
+                startActivity(Intent(this, MainActivityGeo_memory::class.java))
+
+            }
+
         }
     }
 }
+
+
