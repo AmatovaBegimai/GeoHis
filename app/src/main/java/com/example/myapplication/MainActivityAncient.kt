af528a861dfd80d54dd27532b63b351d978217b5
osmonaliev <osmonalievbekzhann@gmail.com>
2021-12-01 15:27:03 +0600

    commit2

--- app/src/main/java/com/example/myapplication/MainActivityAncient.kt
+++ app/src/main/java/com/example/myapplication/MainActivityAncient.kt
@@ -4,19 +4,27 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.ui.AppBarConfiguration
 
 class MainActivityAncient : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_ancient)
         //body
 
         //body
-        val click1 = findViewById<Button>(R.id.but_train)
+        //navbar
+        val navController = findViewById<BottomNavigationView>(R.id.navbar)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.mainmenu
 
-
-        click1.setOnClickListener {
-            startActivity(Intent(this, MainActivityGeoMap::class.java))
-        }
+                    setupActionBarWithNavController(navController, appBarConfiguration)
+                navView.setupWithNavController(navController
     }
-}
+
+
